6fe2642e4a8b680376f482bb0b427636ca5b8266
narissarr-design <narissarr@googlemail.com>
2025-11-06 21:53:21 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: 'cafe-paw-entrance',
+  base: '/cafe-paw-entrance',
   server: {
     host: "::",
     port: 8080,
